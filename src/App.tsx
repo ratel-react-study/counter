41daf601c36611afa9b7779c8eaa9b5069ebeff7
im-wen3y <imwen3y@gmail.com>
2024-09-19 06:38:31 +0900

    feat: 2주차 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import {Container, Heading, HStack } from '@chakra-ui/react';
 import Counter from './components/counter/Counter';
+import Radio from './components/radio/Radio';
+import { useState } from 'react';
+import RadioGroup from './components/radio/RadioGroup';
 
 function App() {
+  const [checked, setChecked] = useState<string>('');
+
+  const handleClickRadioInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {value} = e.target;
+    setChecked(value);
+  }
+
+  console.log({checked});
+
   return (
-    <Counter />
+    <Container>
+      <Heading>카운터 컴포넌트</Heading>
+
+      <HStack spacing={"16px"}>
+        <RadioGroup value={checked} onChange={handleClickRadioInput}>
+          <Radio value={"R"}>R</Radio>
+          <Radio value={"L"}>L</Radio>
+        </RadioGroup>
+      </HStack>
+
+      <HStack spacing={"16px"}>
+        <Counter disabled={checked === 'L'} />
+        <Counter disabled={checked === 'R'} />
+      </HStack>
+    </Container>
   );
 }
 
