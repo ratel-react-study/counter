003afc2cf0a927c4ceede594126305614c217dc0
im-wen3y <imwen3y@gmail.com>
2024-09-18 21:13:23 +0900

    refactor: ♻️ 카운터 컴포넌트 코드 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import Counter from './components/counter/Counter';
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-
-
   return (
-    <>
-      <div>카운트 컴포넌트</div>
-      <div>
-        <span onClick={() => setCount(count - 1)}>-</span>
-        <input type="text" name="count" value={count} />
-        <span onClick={() => setCount(count + 1)}>+</span>
-      </div>
-    </>
+    <Counter />
   );
 }
 
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,20 +1,61 @@
-import * as Styles from "./Counter.style";
+import { useState } from "react";
+import { Box, ButtonGroup, IconButton, Input } from "@chakra-ui/react";
+import { BiMinus, BiPlus } from "react-icons/bi";
 
 type Props = {
-    min: number;
-    max: number;
-    step: number;
-    
-}
+  defaultValue?: number;
+  min?: number;
+  max?: number;
+  step?: number;
+  precision?: number;
+};
 
-const Counter = ({min, max, step}: Props) => {
-    return (
-        <Styles.Container>
-            <Styles.Button>-</Styles.Button>
-            <Styles.Input />
-            <Styles.Button>+</Styles.Button>
-        </Styles.Container>
-    );
+const Counter = ({
+  defaultValue = -4,
+  min = -15,
+  max = 15,
+  step = 0.25,
+  precision = 2,
+}: Props) => {
+  const [count, setCount] = useState<number>(defaultValue);
+
+  const convertFixed = (value: number, precision: number) => {
+    return value.toFixed(precision);
+  };
+
+  const handleClickIncrease = () => {
+    if (count >= max) {
+      alert("최대값 초과 불가능");
+      return;
+    }
+    setCount(count + step);
+  };
+
+  const handleClickDecrease = () => {
+    if (count <= min) {
+      alert("최소값 이하 불가능");
+      return;
+    }
+    setCount(count - step);
+  };
+
+  return (
+    <Box>
+      <ButtonGroup>
+        <IconButton
+          icon={<BiMinus />}
+          onClick={handleClickDecrease}
+          aria-label={"icon-button_minus"}
+        />
+        <Input readOnly value={convertFixed(count, precision)} />
+        <IconButton
+          icon={<BiPlus />}
+          onClick={handleClickIncrease}
+          aria-label={"icon-button_plus"}
+        />
+      </ButtonGroup>
+    </Box>
+  );
 };
 
 export default Counter;
